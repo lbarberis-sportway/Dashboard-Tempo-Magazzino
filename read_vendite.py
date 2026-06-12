7e7b7837c105f8d013ae951e7f1c82c94231aee7
lbarberis-sportway <l.barberis@sportway.net>
2026-06-12 12:37:09 +0200

    refactor: remove redundant read_vendite.py script used for column inspection

--- read_vendite.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import sys
-
-file_path = r"C:\Users\Vstor\Desktop\Ludovico\Analisi Dati\Dashboard-Tempo-Magazzino\VENDITE.xlsx"
-try:
-    df = pd.read_excel(file_path, nrows=5)
-    print("Colonne VENDITE:", list(df.columns))
-except Exception as e:
-    print("Errore:", e)
